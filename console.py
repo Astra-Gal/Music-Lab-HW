483450ddb5d5756ddecbf4d3501d378c420453fd
Astra-Gal <giulia.galastro@gmail.com>
2020-12-08 19:34:49 +0000

    added to album repository

--- console.py
+++ console.py
@@ -5,5 +5,15 @@ from models.artist import Artist
 import repositories.album_repository as album_repository
 import repositories.artist_repository as artist_repository
 
+artist_repository.delete_all()
+album_repository.delete_all()
+
 artist_1 = Artist ("Angel Olsen")
 artist_repository.save(artist_1)
+
+album_1 = Album ("Burn Your Fire For No Witness", "Indie folk", artist_1)
+album_repository.save(album_1)
+
+res = album_repository.select_all()
+for album in res:
+    print(album.__dict__)
