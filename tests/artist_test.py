66ee2f0f3ed4c8ab72e4c44266181fc08f954997
Astra-Gal <giulia.galastro@gmail.com>
2020-12-08 16:30:48 +0000

    second artist test passing

--- tests/artist_test.py
+++ tests/artist_test.py
@@ -4,7 +4,10 @@ from models.artist import Artist
 class TestArtist(unittest.TestCase):
 
     def setUp(self):
-        self.artist = Artist("Angel Olsen", 60)
+        self.artist = Artist("Angel Olsen")
 
     def test_artist_has_name(self):
         self.assertEqual("Angel Olsen", self.artist.name)
+
+    def test_artist_id_starts_none(self):
+        self.assertEqual(None, self.artist.id)
