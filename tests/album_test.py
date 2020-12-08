e642c39210ff7c5b088b6962aa5d1676c7e25cea
Astra-Gal <giulia.galastro@gmail.com>
2020-12-08 16:41:40 +0000

    album tests passed

--- tests/album_test.py
+++ tests/album_test.py
@@ -7,3 +7,12 @@ class TestAlbum(unittest.TestCase):
 
     def test_album_has_title(self):
         self.assertEqual("Burn Your Fire For No Witness", self.album.title)
+
+    def test_album_has_genre(self):
+        self.assertEqual("Indie folk", self.album.genre)
+
+    def test_album_has_artist(self):
+        self.assertEqual("Angel Olsen", self.album.artist)
+
+    def test_album_id_starts_as_none(self):
+        self.assertEqual(None, self.album.id)
