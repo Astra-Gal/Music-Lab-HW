656efc823ca0e9df2eb48b7894f7e7336ed08554
Astra-Gal <giulia.galastro@gmail.com>
2020-12-08 19:43:14 +0000

    completed MVP

--- console.py
+++ console.py
@@ -11,8 +11,14 @@ album_repository.delete_all()
 artist_1 = Artist ("Angel Olsen")
 artist_repository.save(artist_1)
 
+artist_2 = Artist ("Led Zeppelin")
+artist_repository.save(artist_2)
+
 album_1 = Album ("Burn Your Fire For No Witness", "Indie folk", artist_1)
 album_repository.save(album_1)
+
+album_2 = Album ("Led Zeppelin IV", "Rock", artist_2)
+album_repository.save(album_2)
 
 res = album_repository.select_all()
 for album in res:
